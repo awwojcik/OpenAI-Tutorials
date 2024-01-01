9b27c124d9806428704393ac0971a737326a44be
awwojcik <125920669+awwojcik@users.noreply.github.com>
2024-01-01 19:01:06 +0100

    Update meeting_minutes.py

--- meeting_minutes.py
+++ meeting_minutes.py
@@ -1,1 +1,12 @@
+from opeanai import OpenAI
 
+client = OpenAI(
+  os.environ.get("OPENAI_API_KET")
+  api_key="My API Key"
+  )
+from docx import Document
+
+def transkrybuj_glos(audio_file_path):
+  with open(audio_file_path, 'rb') as audio_file:
+    transkrybcja = client.audio.transcriptions.create("whisper-1",audio_file)
+  return transkrybcja['text']
